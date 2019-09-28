61f6d4efedf582e4265c92c0b38a75a210a81863
Wesley Matos <wrickee@gmail.com>
2019-09-28 01:32:52 -0300

    add middleware to validate body of login

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,7 +1,9 @@
 const { auth } = require('./controller');
+const contracts = require('./contracts');
+const validator = require('../../middlewares/validator');
 
 module.exports = (router) => {
-  router.post('/login', async (req, res, next) => {
+  router.post('/login', validator(contracts.auth, 'body'), async (req, res, next) => {
     try {
       const { body } = req;
       const data = await auth(body);
