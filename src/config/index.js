02c1188b68402e3230d94ffa5e2a91ed2a186835
Wesley Matos <wrickee@gmail.com>
2019-09-27 21:34:02 -0300

    freeze object of envs

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-module.exports = {
+module.exports = Object.freeze({
   PORT: process.env.PORT || 3000,
   SECRET: process.env.SECRET,
   NODE_ENV: process.env.NODE_ENV,
   MONGODB_URI: process.env.MONGODB_URI,
   MONGODB_DATABASE: process.env.MONGODB_DATABASE,
   TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN,
-};
+});
