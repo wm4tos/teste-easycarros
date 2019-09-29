e983fa437e04fb8666ad7620936334e91e90827d
Wesley Matos <wrickee@gmail.com>
2019-09-28 23:27:43 -0300

    remove tests

--- test/example.test.js
+++
@@ -1,8 +0,0 @@
-// const { expect } = require('chai');
-// const mongoUnit = require('mongo-unit');
-
-// const { MONGO_URL } = require('../src/config');
-
-// describe('Descrição do teste', () => {
-//   beforeEach(() => mongoUnit.initDb(MONGO_URL, { seu banco de dados }));
-// });
--- test/it-helper.js
+++
@@ -1,8 +0,0 @@
-const prepare = require('mocha-prepare');
-const mongoUnit = require('mongo-unit');
-
-prepare(done => mongoUnit.start()
-  .then((mongoURL) => {
-    process.env.MONGO_URL = mongoURL;
-    done();
-  }));
